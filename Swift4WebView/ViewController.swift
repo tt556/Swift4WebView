224a416383ac098172dbefe88b40ff6ed9ad3091
神崎泰旗 <taikikanzaki@mbp.local>
2018-08-20 23:30:22 +0900

    Initial Commit

---
+++ Swift4WebView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift4WebView
+//
+//  Created by 神崎泰旗 on 2018/08/20.
+//  Copyright © 2018年 taiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
