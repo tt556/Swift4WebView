2e7da51e43e7a47cc00fada44313a996ff2bf4dc
tt556 <so888tk@gmail.com>
2018-11-07 01:45:14 +0900

    second commit

--- Swift4WebView/WebView2Controller.swift
+++ Swift4WebView/WebView2Controller.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WebView2Controller.swift
 //  Swift4WebView
 //
 //  Created by 神崎泰旗 on 2018/08/20.
@@ -8,16 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIWebViewDelegate {
+class WebView2Controller: UIViewController, UIWebViewDelegate {
 
     @IBOutlet var webView: UIWebView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         webView.delegate = self
-        let url = URL(string: "http://www.yahoo.co.jp")
+        let url = URL(string: "http://www.mbga.jp/")
         let urlRequest = URLRequest(url: url!)
         webView.loadRequest(urlRequest)
 
@@ -33,13 +32,23 @@ class ViewController: UIViewController, UIWebViewDelegate {
         
         //インディケーター終了
         UIApplication.shared.isNetworkActivityIndicatorVisible = false
-
+        
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
--- Swift4WebView/WebViewController.swift
+++ Swift4WebView/WebViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WebViewController.swift
 //  Swift4WebView
 //
 //  Created by 神崎泰旗 on 2018/08/20.
@@ -8,20 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIWebViewDelegate {
+class WebViewController: UIViewController, UIWebViewDelegate {
 
     @IBOutlet var webView: UIWebView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         webView.delegate = self
-        let url = URL(string: "http://www.yahoo.co.jp")
+        let url = URL(string: "http://www.google.com")
         let urlRequest = URLRequest(url: url!)
         webView.loadRequest(urlRequest)
 
+
     }
+
     func webViewDidStartLoad(_ webView: UIWebView) {
         
         //インディケーターを開始
@@ -33,13 +34,23 @@ class ViewController: UIViewController, UIWebViewDelegate {
         
         //インディケーター終了
         UIApplication.shared.isNetworkActivityIndicatorVisible = false
-
+        
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
